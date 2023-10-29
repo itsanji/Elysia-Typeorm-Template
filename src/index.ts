394f25ab9e96f73e66eba80e255353647775bd01
anjiboss <nhu.nhu.face@gmail.com>
2023-10-29 20:01:08 +0900

    logging to file, inprogress

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,27 @@ import { AppDataSource } from "./data-source";
 import { userController } from "./controllers/user.controller";
 import cors from "@elysiajs/cors";
 import { logger } from "@grotto/logysia";
+import path from "path";
 import "reflect-metadata";
 import swagger from "@elysiajs/swagger";
+import fs from "fs";
 
+let oldLogger = console.log;
+let newLogger = (...data: any[]) => {
+    const c = data.slice(-1);
+    if (typeof c === "boolean" && c === true) {
+        oldLogger(data);
+    } else {
+        fs.appendFileSync(
+            path.join(__dirname, "..", "logs", "test.log"),
+            `${JSON.stringify(data)}\n`
+        );
+    }
+};
+
+console.log = newLogger;
+
+await Bun.write(path.join(__dirname, "..", "logs", "test.log"), "testing");
 async function server() {
     const db = await AppDataSource.initialize();
     const app = new Elysia()
