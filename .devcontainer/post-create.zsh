4186a572d1fdb121720a5005a65d303d8f0ac38e
Anji <nhu.nhu.face@gmail.com>
2023-10-30 15:59:07 +0900

    add install mysql script

--- .devcontainer/post-create.zsh
+++ .devcontainer/post-create.zsh
@@ -1,2 +1,9 @@
 #!/bin/zsh
 echo "Looks like everything works as expected!"
+
+echo "Installing Mysql"
+sudo apt update \
+  && sudo apt install -y mysql-server \
+  && sudo service mysql start \
+  && echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'" | sudo mysql \
+  && sudo service --status-all 
